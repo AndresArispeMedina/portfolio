91bc1aed88a5ee6ae71be7b3bf5a537f2fc4b1a8
Andres Arispe <andres.arispe@ascbominimac87.local>
2022-03-08 00:59:57 -0400

    finishing responsive styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -10,9 +12,11 @@ const Header = () => {
         <h1>Andres Arispe</h1>
         <h5 className='text-light'>Full Stack Developer</h5>  
         <CTA />
+        <HeaderSocials />
         <div className="me">
-          <img />  
+          <img src={ME} alt=""/>  
         </div>
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
